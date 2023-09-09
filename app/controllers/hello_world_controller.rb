8155ed7c887e8d5937cf9008be7c609b5fd84588
ben durfee <ben.p.durfee@gmail.com>
2023-09-09 05:38:50 -0700

    initial config

--- app/controllers/hello_world_controller.rb
+++ app/controllers/hello_world_controller.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
+# defined hello world controller
+# subclass < superclass
+# ApplicationController class inherits functionality from ActionController::Base 
 
 class HelloWorldController < ApplicationController
   layout "hello_world"
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
+# this block wraps all routes
+# controllers in snake_case
+# controller file itself in snake_case and suffixed with _controller.rb
+# 
+
 Rails.application.routes.draw do
-  get 'hello_world', to: 'hello_world#index'
+  # get 'hello_world', to: 'hello_world#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Defines the root path route ("/") to be handled by hello_world_controller.rb action index
+  root 'hello_world#index'
 end
+
