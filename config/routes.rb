94e3b52f928717bcf6fff0f46de121309a7496e1
ben durfee <ben.p.durfee@gmail.com>
2023-09-09 05:39:25 -0700

    cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # get 'hello_world', to: 'hello_world#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/") to be handled by hello_world_controller.rb action index
+  # Defines the root path route "/" to be handled by hello_world_controller.rb action index
   root 'hello_world#index'
 end
 
