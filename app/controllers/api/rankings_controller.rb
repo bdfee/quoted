95141d7780346fa1fda81f2e5e660972bcd3b5cc
ben durfee <ben.p.durfee@gmail.com>
2023-09-14 13:59:36 -0700

    cleanup

--- app/controllers/api/rankings_controller.rb
+++ app/controllers/api/rankings_controller.rb
@@ -1,10 +1,9 @@
 module Api
     class RankingsController < ApplicationController
         def index
-        # Join the scores and users tables and order by ranking_score in descending order
+            
         @rankings = Score.joins(:user).order(ranking_score: :desc)
   
-        # Select the fields you want to include in the response (ranking_score and username)
         response_data = @rankings.map do |ranking|
           {
             ranking_score: ranking.ranking_score,
