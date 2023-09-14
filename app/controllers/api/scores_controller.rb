d37434594e3739d973ebf7f597f39fdae405a770
ben durfee <ben.p.durfee@gmail.com>
2023-09-14 10:12:29 -0700

    organize controller and service for scoring

--- app/controllers/api/scores_controller.rb
+++ app/controllers/api/scores_controller.rb
@@ -1,38 +1,36 @@
 module Api
     class ScoresController < ApplicationController
-      require 'jwt'
+        require 'jwt'
       
-      def increment
-
-        # Check for JWT with userID, which indicates logged-in status
-        token = request.headers['Authorization']&.split(' ')&.last
+        def increment
+            # Check for JWT with userID, which indicates logged-in status
+            token = request.headers['Authorization']&.split(' ')&.last
   
-        # If present, decode for userID
-        payload = token.present? ? JWT.decode(token, ENV['JWT_SECRET'], true, algorithm: 'HS256') : nil
+            # If present, decode for userID
+            payload = token.present? ? JWT.decode(token, ENV['JWT_SECRET'], true, algorithm: 'HS256') : nil
   
-        # safe navigation to user_id, will be nil if user not logged in
-        user_id = payload&.dig(0, 'user_id')
-
-        score_type = params[:score_type]
-
-        # score service can take nil as user_id to only update global score
-        score_service = ScoreService.new(score_type, user_id)
-
-        score_service.increment_global_score
-
-        if user_id.present?
-            
-            score_service.increment_user_score
-            
-            ActionCable.server.broadcast("scores_channel", { user_score: score, global_score: global_score })
-            
-            render json: { message: 'Score updated' }, status: :ok
-        else
-            
-            ActionCable.server.broadcast("scores_channel", { global_score: global_score })
+            # Safe navigation to user_id, will be nil if user not logged in
+            user_id = payload&.dig(0, 'user_id')
   
-            render json: { message: 'Global score updated' }, status: :ok
+            score_type = params[:score_type]
+  
+            # Score service can take nil as user_id to only update global score
+            score_service = ScoreService.new(score_type, user_id)
+            # increment the global score
+            global_score = score_service.increment_global_score
+  
+            if user_id.present?
+                #increment the user score
+                user_score = score_service.increment_user_score
+                #Socket the user and global score
+                ActionCable.server.broadcast("scores_channel", { user_score: user_score, global_score: global_score })
+
+                render json: { message: 'Score updated' }, status: :ok
+            else
+                #socket the global score only
+                ActionCable.server.broadcast("scores_channel", { global_score: global_score })
+                render json: { message: 'Global score updated' }, status: :ok
+            end
         end
-
     end
 end
--- app/services/score_service.rb
+++ app/services/score_service.rb
@@ -3,37 +3,44 @@ class ScoreService
         @score_type = score_type
         @user_id = user_id
     end
-
+  
     def increment_global_score
         global_score = GlobalScore.first_or_initialize
-
+  
         if @score_type == 'correct'
             global_score.increment!(:correct_responses)
         elsif @score_type == 'incorrect'
             global_score.increment!(:incorrect_responses)
         end
-
-        global_score_save
+  
+        global_score.save
+  
+        # Return the updated global_score
+        global_score
     end
-    
+  
     def increment_user_score
         return unless @user_id
-    
+  
         score = Score.find_or_initialize_by(user_id: @user_id)
-    
+  
         if @score_type == 'correct'
             score.increment!(:correct_responses)
         elsif @score_type == 'incorrect'
             score.increment!(:incorrect_responses)
         end
-    
+  
+        # we calcualte and store the ranking on each DB write for easy query of leaderboard
         score.ranking_score = calculate_ranking_score(score)
-    
+  
         score.save
-      end
-
+  
+        # Return the updated user score
+        score
+    end
+  
     def calculate_ranking_score(score)
         correct_percentage = score.correct_responses.to_f / (score.correct_responses + score.incorrect_responses)
         score.correct_responses * correct_percentage
     end
-end
+  end
