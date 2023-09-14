3b14cf0b9194fc5b4b7d437673bb750c6c44e479
ben durfee <ben.p.durfee@gmail.com>
2023-09-14 09:57:21 -0700

    split out score service

--- app/controllers/api/scores_controller.rb
+++ app/controllers/api/scores_controller.rb
@@ -3,70 +3,36 @@ module Api
       require 'jwt'
       
       def increment
-        # Global scoring regardless of logged-in status
-        global_score = GlobalScore.first_or_initialize
-  
-        case params[:score_type]
-        when 'correct'
-          global_score.increment!(:correct_responses)
-        when 'incorrect'
-          global_score.increment!(:incorrect_responses)
-        end
-  
-        global_score.save
-  
+
         # Check for JWT with userID, which indicates logged-in status
         token = request.headers['Authorization']&.split(' ')&.last
   
         # If present, decode for userID
         payload = token.present? ? JWT.decode(token, ENV['JWT_SECRET'], true, algorithm: 'HS256') : nil
   
-        if payload.present?
-          user_id = payload[0]['user_id']
-          user = User.find_by(id: user_id)
-  
-          if user
-            # Increment the user's score
-            score = Score.find_or_initialize_by(user: user)
-  
-            case params[:score_type]
-            when 'correct'
-              score.increment!(:correct_responses)
-            when 'incorrect'
-              score.increment!(:incorrect_responses)
-            else
-              render json: { error: 'Invalid score type' }, status: :bad_request
-              return
-            end
-            
-            # Update ranking score
-            score.ranking_score = calculate_ranking_score(score)
-  
-            score.save
-  
-            # Send scores on the socket to the nav bar in the client
-            ActionCable.server.broadcast("scores_channel", { user_score: score, global_score: global_score })
-  
-            render json: { message: 'Score updated' }, status: :ok
-          else
-            render json: { error: 'User not found' }, status: :not_found
-          end
-        else
-          # Send global score only to the nav in the client
-          ActionCable.server.broadcast("scores_channel", { global_score: global_score })
-  
-          render json: { message: 'Global score updated' }, status: :ok
-        end
-      end
-  
-      private
-  
-      def calculate_ranking_score(score)
-        # percentage correct * number correct
-        correct_percentage = score.correct_responses.to_f / (score.correct_responses + score.incorrect_responses)
-        ranking_score = score.correct_responses * correct_percentage
+        # safe navigation to user_id, will be nil if user not logged in
+        user_id = payload&.dig(0, 'user_id')
 
-        return ranking_score
-      end
+        score_type = params[:score_type]
+
+        # score service can take nil as user_id to only update global score
+        score_service = ScoreService.new(score_type, user_id)
+
+        score_service.increment_global_score
+
+        if user_id.present?
+            
+            score_service.increment_user_score
+            
+            ActionCable.server.broadcast("scores_channel", { user_score: score, global_score: global_score })
+            
+            render json: { message: 'Score updated' }, status: :ok
+        else
+            
+            ActionCable.server.broadcast("scores_channel", { global_score: global_score })
+  
+            render json: { message: 'Global score updated' }, status: :ok
+        end
+
     end
-  end
+end
